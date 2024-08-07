faaab541d417b3766d72c4fd2c466738ab5160e9
Krshnaprasd <krxhnapraxad@gmail.com>
2024-08-07 12:57:16 +0530

    corrections fixed

---
+++ src/Demo.jsx
@@ -0,0 +1,31 @@
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+
+// const Demo = () => {
+//     const [users, setUsers] = useState([]);
+
+//     useEffect(() => {
+//         axios ("https://dummyjson.com/users")
+//             .then(response => response.data)
+//             .then(data => {
+               
+//                 setUsers(data.users);
+//                 console.log(data);
+                
+//             })
+//             .catch(err => {
+//                 console.error(err); 
+//             });
+//     }, []);
+
+//     return (
+//         <>
+//             {users.map((user,index) => (
+//                 <p key={user.id}>{index + 1}. {user.firstName} {user.lastName}
+//                 </p>
+//             ))}
+//         </>
+//     );
+// }
+
+// export default Demo;
